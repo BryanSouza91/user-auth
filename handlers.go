277fcb89670a55f7c9ca4649913348e2e181e29d
Bryan Souza <brybryflys@gmail.com>
2022-08-30 23:51:52 -0700

    closes gh-1; made use of redis cache for session authentication

--- handlers.go
+++ handlers.go
@@ -8,9 +8,10 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gomodule/redigo/redis"
+	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -35,51 +36,67 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Println(fmt.Sprintf("Success! %s is registered.", creds.Username))
+	return
 }
 
 func Signin(w http.ResponseWriter, r *http.Request) {
 	creds := &Credentials{}
+	storedCreds := &Credentials{}
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	err := json.NewDecoder(r.Body).Decode(&creds)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
-	}
-
-	storedCreds := &Credentials{}
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	err = db.Collection(COLLECTION).FindOne(ctx, bson.D{{Key: "username", Value: creds.Username}}).Decode(&storedCreds)
-	defer cancel()
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Println(fmt.Sprintf("no matching username: %s", creds.Username))
-			return
-		}
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Fatal(err)
-	}
-
-	if err = bcrypt.CompareHashAndPassword([]byte(storedCreds.Password), []byte(creds.Password)); err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		fmt.Println(fmt.Sprintf("%s unauthorized", creds.Username))
 	} else {
-		sessionToken, err := uuid.New()
+		storedSessionCookie, err := r.Cookie("session_token")
 		if err != nil {
+			fmt.Println(fmt.Sprintf("No active session found for %s", creds.Username))
+		} else {
+			storedSessionToken := storedSessionCookie.Value
+			cachedSessionUsername, err := redis.String(cache.Do("GET", storedSessionToken))
+			if err != nil {
+				fmt.Println(fmt.Sprintf("No active session found for %s", creds.Username))
+			} else if cachedSessionUsername == creds.Username {
+				fmt.Println(fmt.Sprintf("Success! %s is authorized via session token.", cachedSessionUsername))
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+		}
+		err = db.Collection(COLLECTION).FindOne(ctx, bson.D{{Key: "username", Value: creds.Username}}).Decode(&storedCreds)
+		defer cancel()
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				w.WriteHeader(http.StatusNotFound)
+				fmt.Println(fmt.Sprintf("no matching username: %s", creds.Username))
+				return
+			}
 			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatal(err)
 			return
 		}
-		_, err = cache.Do("SETEX", sessionToken, "120", creds.Username)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+		if err = bcrypt.CompareHashAndPassword([]byte(storedCreds.Password), []byte(creds.Password)); err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Println(fmt.Sprintf("%s unauthorized", creds.Username))
+			return
+		} else {
+			sessionToken := uuid.NewString()
+			_, err = cache.Do("SETEX", sessionToken, 120, creds.Username)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			http.SetCookie(w, &http.Cookie{
+				Name:    "session_token",
+				Value:   fmt.Sprint(sessionToken),
+				Expires: time.Now().Add(120 * time.Second),
+			})
+			w.WriteHeader(http.StatusOK)
+			fmt.Println(fmt.Sprintf("Session token: %s", sessionToken))
+			fmt.Println(fmt.Sprintf("Success! %s is authorized.", creds.Username))
 			return
 		}
-
-		http.SetCookie(w, &http.Cookie{
-			Name:    "session_token",
-			Value:   fmt.Sprint(sessionToken),
-			Expires: time.Now().Add(120 * time.Second),
-		})
-		fmt.Println(fmt.Sprintf("Success! %s is authorized.", creds.Username))
 	}
 
 }
